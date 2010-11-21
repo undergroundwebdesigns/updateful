e8ebd37d4c6acda78538b3d48b1f5960e26b1afd
Alex Willemsma <webmaster@undergroundwebdesigns.com>
2010-11-21 14:38:07 +1300

    Initial Rails outline.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Updateful::Application.config.secret_token = '5cd49a777187b82a031a09dafb0fceb4862a70cec9ff2c04f72844a1b9fabfd447f67f73c6edac43ebc9c8b7c77a72a64394868e3ae9100764d7eb7589280908'
