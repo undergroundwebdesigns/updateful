af6a84327a84bc3296b36229e815a679c0c5e217
Alex Willemsma <webmaster@undergroundwebdesigns.com>
2010-12-13 18:23:53 +1300

    Moving application contents into applications/web

--- public/index.php
+++ public/index.php
@@ -6,7 +6,7 @@
  *
  * @see  http://kohanaframework.org/guide/about.install#application
  */
-$application = '../application';
+$application = '../applications/web';
 
 /**
  * The directory in which your modules are located.
