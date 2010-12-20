1de16a58c4513a38b26e2582cf892532b997b704
Ben Hanna <ben@benhanna.com>
2010-12-20 22:43:49 +1300

    ignoring log and cache files
    Signed-off-by: Ben Hanna <ben@benhanna.com>

--- applications/web/logs/2010/12/17.php
+++
@@ -1,3 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.'); ?>
-
-2010-12-17 04:21:37 --- ERROR: ReflectionException [ -1 ]: Class controller_updateful does not exist ~ SYSPATH/classes/kohana/request.php [ 1094 ]
