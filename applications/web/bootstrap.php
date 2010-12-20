03d265fd2ccda5c71f2f2c6201bd63b7e566481d
Alex Willemsma <webmaster@undergroundwebdesigns.com>
2010-12-20 22:41:02 +1300

    Removing un-needed slash from production and testing environments.

--- applications/web/bootstrap.php
+++ applications/web/bootstrap.php
@@ -71,7 +71,7 @@ else
 if (Kohana::$environment === Kohana::DEVELOPMENT)
 {
 	$init_settings = array(
-		'base_url' => '/',
+		'base_url' => '',
 		'index_file' => '', // Used so that URLs are created correctly, without referencing index.php
 	);
 }
@@ -85,7 +85,7 @@ elseif (Kohana::$environment === 'development_ben')
 elseif (Kohana::$environment === Kohana::TESTING)
 {
 	$init_settings = array(
-		'base_url' => '/',
+		'base_url' => '',
 		'index_file' => '', // Used so that URLs are created correctly, without referencing index.php
 	);
 	
@@ -94,7 +94,7 @@ elseif (Kohana::$environment === Kohana::TESTING)
 else // Production settings... these are the defaults if the above test fails, since they should be the strictest:
 {
 	$init_settings = array(
-		'base_url' => '/',
+		'base_url' => '',
 		'profile' => FALSE,
 		'caching' => TRUE,
 		'index_file' => '', // Used so that URLs are created correctly, without referencing index.php
